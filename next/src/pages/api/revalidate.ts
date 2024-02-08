33e0a6ee3b50f8236cef48df40cb35e44cd7209f
Omari <marzipant.e@gmail.com>
2024-02-08 10:33:35 +0000

    Add printing

--- next/src/pages/api/revalidate.ts
+++ next/src/pages/api/revalidate.ts
@@ -4,6 +4,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    console.log(req.body);
     // Check for secret to confirm this is a valid request
     if (req.query.token !== process.env.REVALIDATE_TOKEN) {
         return res.status(401).json({ message: 'Invalid token' });
