8592e683441a02057bf8e26acb68b69b7e425dc2
Omari <marzipant.e@gmail.com>
2024-02-22 00:18:54 +0000

    Add related posts to page

--- next/src/pages/blog/[slug].tsx
+++ next/src/pages/blog/[slug].tsx
@@ -17,23 +17,49 @@ import typescript from 'highlight.js/lib/languages/typescript';
 import { ReactElement, useEffect, useState } from 'react';
 
 import { ArticleJsonLd, NextSeo } from 'next-seo';
-import { Post } from 'types';
-import { STRAPI_TOKEN, getPost } from '@/utils/strapi';
+import { Post, PostBrief, WithChildrenProps } from 'types';
+import { STRAPI_TOKEN, fetchArticleBriefs, getPost } from '@/utils/strapi';
 import Image from 'next/image';
 import { ArticleEnding, AuthorTag, BlogLayout } from '@/components/blog';
 import Link from 'next/link';
+import { Dot } from '@/components/Dot';
+import { cn } from '@/utils/index';
 const STRAPI_URL = process.env.STRAPI_URL ?? 'https://cms.marileon.me';
-
+function FormattedHeading({ children }: WithChildrenProps) {
+    if (typeof children !== 'string') return children;
+    if (children.endsWith('?') || children.endsWith('.')) {
+        const start = children.slice(0, children.length - 1);
+        const end = children.at(-1);
+        return (
+            <>
+                {start}
+                <span className="text-primary">{end}</span>
+            </>
+        );
+    }
+    return (
+        <>
+            {children}
+            <Dot />
+        </>
+    );
+}
 const components: MDXRemoteProps['components'] = {
     a: ({ href, target, children }) => (
         <Link href={href!} target={target}>
             {children}
         </Link>
     ),
+    h2: ({ className, children, ...props }) => (
+        <h2 {...props} className={cn(className, 'lowercase')}>
+            <FormattedHeading>{children}</FormattedHeading>
+        </h2>
+    ),
 };
 hljs.registerLanguage('typescript', typescript);
 export default function Post({
     post,
+    relatedPosts,
     og,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
     const date = new Date(post.date);
@@ -87,7 +113,7 @@ export default function Post({
                 isAccessibleForFree={true}
             />
 
-            <article className="prose prose-sm w-full px-5 py-8 font-body dark:prose-invert md:prose-base lg:prose-lg prose-headings:font-title prose-h1:mb-0 prose-a:transition-all prose-img:mx-auto prose-img:max-w-[60%] prose-img:first-of-type:my-0 prose-a:hover:underline md:max-w-screen-md">
+            <article className="prose prose-sm w-full px-5 py-8 font-body dark:prose-invert md:prose-base lg:prose-lg marker:text-primary prose-headings:font-title prose-h1:mb-0 prose-a:transition-all prose-img:mx-auto prose-img:max-w-[60%] prose-img:first-of-type:my-0 prose-a:hover:underline md:max-w-screen-md">
                 <ul className="flex w-full items-center justify-center gap-8">
                     {post.categories.map(({ name, slug }) => (
                         <span className="font-bold text-primary" key={slug}>
@@ -98,7 +124,6 @@ export default function Post({
                 <div className="flex w-full flex-col gap-2">
                     <h1>{post.title}</h1>
                     <AuthorTag />
-
                     <div className="flex flex-row text-sm">
                         {date.toDateString()} •{' '}
                         {Math.round(post.readingTime.minutes)} {suffix}
@@ -107,6 +132,21 @@ export default function Post({
 
                 <div className="relative">
                     <MDXRemote {...post.content} components={components} />
+                </div>
+                <div>
+                    <h2>
+                        read more
+                        <Dot />
+                    </h2>
+                    <ul>
+                        {relatedPosts.map(({ title, slug }) => (
+                            <li key={slug}>
+                                <Link href={`/blog/${slug}`} className="">
+                                    {title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
                 <ArticleEnding />
             </article>
@@ -142,11 +182,13 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps<{
     post: Post;
+    relatedPosts: PostBrief[];
     og: string;
 }> = async ({ params }) => {
     const slug = params?.slug as string;
 
     const post = await getPost(slug);
+    const { articles: relatedPosts } = await fetchArticleBriefs(0, 6);
 
     if (!post) {
         console.log(`Post ${slug} not found.`);
@@ -180,6 +222,9 @@ export const getStaticProps: GetStaticProps<{
                 })),
                 readingTime: rt,
             },
+            relatedPosts: relatedPosts
+                .filter((p) => p.slug !== slug)
+                .slice(0, 5),
             og: ogLink.toString(),
             revalidate: 3600,
         },
